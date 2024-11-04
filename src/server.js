326791116523512d39792c0b863b651845a6d04c
Pablo <dexpb@hotmail.com>
2024-11-04 19:12:30 -0300

    feat: ""

--- src/server.js
+++ src/server.js
@@ -1,15 +1,18 @@
-const express = require("express")
-const routes = require("./routes")
-const app = express()
-const cors =  require("cors")
-const port = process.env.PORT || 3333;
+const express = require("express");
+const routes = require("./routes");
+const cors = require("cors");
 
-app.use(cors())
-app.use(express.json())
-app.use(routes)
-app.get("/tarefas", (req, res) => {
-})
+const app = express();
 
-app.listen(port, () => console.log(`Server running on port ${port}`))
+app.use(cors());
+app.use(express.json());
+app.use("/api", routes);
 
-module.exports = app
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
+
+module.exports = app;
