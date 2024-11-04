d6350afe15e17103a17fb913d00bb90deec3e25f
Pablo <dexpb@hotmail.com>
2024-11-04 19:58:24 -0300

    feat: "3"

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-
+const PORT = process.env.PORT || 3333;
 const express = require("express")
 const routes = require("./routes")
 const app = express()
@@ -7,7 +7,5 @@ const cors =  require("cors")
 app.use(cors())
 app.use(express.json())
 app.use(routes)
-app.get("/tarefas", (req, res) => {
-})
 
-app.listen(3333, () => console.log("server up 3333"))
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
