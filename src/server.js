ab1c8573b18e63af4a1e4c5040960d3e970618fd
Pablo <dexpb@hotmail.com>
2024-11-04 12:43:40 -0300

    feat:

--- src/server.js
+++ src/server.js
@@ -1,16 +1,12 @@
 const express = require("express")
 const routes = require("./routes")
-const cors = require("cors")
-
 const app = express()
+const cors =  require("cors")
+
 app.use(cors())
 app.use(express.json())
 app.use(routes)
-
 app.get("/tarefas", (req, res) => {
-  // Insira aqui a lógica para responder ao endpoint /tarefas
-  res.status(200).send("Endpoint /tarefas funcionando")
 })
 
-// Em vez de app.listen, exporte o app como uma função
-module.exports = app
+app.listen(3333, () => console.log("server up 3333"))
