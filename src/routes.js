6075bb8546bfc37b29f07aa21dbd8eaff4827fec
Pablo <dexpb@hotmail.com>
2024-11-04 15:43:05 -0300

    feat : ""

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const routes = express.Router();
 const { PrismaClient } = require("@prisma/client");
+
 const prisma = new PrismaClient();
 
 // Create a new task
