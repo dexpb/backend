057e51cedcbb2c8fb2c011ffc1af51769c586faf
Pablo <dexpb@hotmail.com>
2024-11-04 19:44:29 -0300

    feat: "resolved problems with package.json"

--- src/server.js
+++ src/server.js
@@ -1,18 +1,13 @@
-const express = require("express");
-const routes = require("./routes");
-const cors = require("cors");
 
-const app = express();
+const express = require("express")
+const routes = require("./routes")
+const app = express()
+const cors =  require("cors")
 
-app.use(cors());
-app.use(express.json());
-app.use("/api", routes);
+app.use(cors())
+app.use(express.json())
+app.use(routes)
+app.get("/tarefas", (req, res) => {
+})
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server running on port ${port}`));
-
-module.exports = app;
+app.listen(3333, () => console.log("server up 3333"))
