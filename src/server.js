d954ba0239ffd6ff4d249f23ca46b16c6688f1ca
Pablo <dexpb@hotmail.com>
2024-11-04 15:30:11 -0300

    feat ""

--- src/server.js
+++ src/server.js
@@ -2,7 +2,6 @@ const express = require("express")
 const routes = require("./routes")
 const app = express()
 const cors =  require("cors")
-const port = process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.json())
@@ -10,6 +9,6 @@ app.use(routes)
 app.get("/tarefas", (req, res) => {
 })
 
-app.listen(port, () => console.log("server up 3333"))
+app.listen(3000, () => console.log("server up 3333"))
 
 module.exports = app
